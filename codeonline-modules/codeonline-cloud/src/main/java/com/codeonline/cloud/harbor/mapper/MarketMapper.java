8b4070e110679d3ebfd9a43f01eb760869649813
zhangcheng <2813047449@qq.com>
2023-02-26 07:31:12 +0800

    环境放到服务器上

--- codeonline-modules/codeonline-cloud/src/main/java/com/codeonline/cloud/harbor/mapper/MarketMapper.java
+++ codeonline-modules/codeonline-cloud/src/main/java/com/codeonline/cloud/harbor/mapper/MarketMapper.java
@@ -16,6 +16,6 @@ public interface MarketMapper {
     /*
     * 查询所有公开的镜像信息，按照创建时间倒序排列，按镜像名称分组，取每组的第一条
     * */
-    @Select("select user_id, image_name, image_tag, image_url,introduce, create_time,update_by,create_by, update_time from harbor_upload where is_public = 1 group by image_name order by create_time desc")
+    @Select("select any_value(user_id) user_id, image_name, any_value(image_tag) image_tag, any_value(image_url) image_url,any_value(introduce) introduce, any_value(create_time) create_time,any_value(update_by) update_by,any_value(create_by) create_by, any_value(update_time) update_time from harbor_upload where is_public = 1 group by image_name order by any_value(create_time) desc")
     List<HarborUpload> getAllMarketImages();
 }
